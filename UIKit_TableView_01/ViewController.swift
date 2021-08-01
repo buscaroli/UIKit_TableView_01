c922857884ff0fe2fc4a8abe8498c6c31570e85b
mabus <m.buscaroli@outlook.com>
2021-08-01 11:37:54 +0100

    Initial Commit

---
+++ UIKit_TableView_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_TableView_01
+//
+//  Created by Matteo on 01/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
